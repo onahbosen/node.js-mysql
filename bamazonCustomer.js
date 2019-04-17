c2c255a10dd613cd21a4ac39ab68983bdd7f1958
onahbosen <onahbosen@gmail.com>
2019-04-17 15:41:58 -0500

    done

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,44 +1,61 @@
-var inquirer = require("inquirer");
-var mysql = require("mysql");
+var mysql = require('mysql');
+var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
-    port: 8889,
-    user: 'root',
-    password: 'root',
-    database: 'bamazon'
+    host: "localhost",
+    port: "8889",
+    user: "root",
+    password: "root",
+    database: "bamazon"
 });
 
-connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
+connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    console.log("item \tproduct \tprice");
+    for (var i = 0; i < res.length; i++) {
+        console.log(res[i].item + " \t" + res[i].product + "\t" + res[i].price);
     }
-    console.log('connected as id ' + connection.threadId);
+
+    inquirer.prompt([
+        {
+        name: "item",
+        type: "input",
+        message: "what item would u like to purchase? [don't want anything? press X]"
+        },
+        {
+            name: "qty",
+            type: "input",
+            message: "how many would u like to purchase?"
+
+        }
+        ]).then(function (productObj) {
+        if (productObj.item.toUpperCase() == "X") {
+            connection.end();
+        } else {
+            connection.query('SELECT * FROM products WHERE ?', { item: productObj.item }, function (err, res) {
+                if (err) throw err;
+                // console.log(res)
+                if (res[0].stock > productObj.qty) {
+
+                    var cost = res[0].price * productObj.qty
+                    console.log("thanks for ur order! ur total is " + cost);
+
+                    var newQty = res[0].stock - productObj.qty
+
+                    connection.query("UPDATE products SET ? WHERE ?", [{
+                            stock: newQty
+                        },
+                            {
+                                item: productObj.item
+                            }],
+
+                        function (err, res) {
+                        });
+                }
+                else {
+                    console.log("sorry, we don't have that many! \nwe only have " + res[0].stock + ". \nplease try again.")
+                }
+            })
+        }
+    })
 });
-
-function shop(){
-    inquirer
-        .prompt([
-            {
-                type: "confirm",
-                message: "do u wanna see what we have in stock??",
-                name: "confirm",
-                default: true
-            }
-        ])
-        .then(function(inquirerResponse) {
-            if (inquirerResponse.confirm) {
-                console.log("ok!");
-                console.log(connection.query('SELECT * FROM products', (err,rows) => {
-                    if(err) throw err;
-                    console.log('Data received from Db:\n');
-                    console.log(rows);
-                }));
-            }
-            else {
-                console.log("ok nvm!");
-            }
-        });
-};
-
-shop();
